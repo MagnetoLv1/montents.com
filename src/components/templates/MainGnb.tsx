cc92a9259a0633cf056729b7d082ef4c1fde0791
wtae1216 <wtae1216@gmail.com>
2020-06-30 00:55:58 +0900

    MainGnb 수정  - LeftPanel 스타일 추가

--- src/components/templates/MainGnb.tsx
+++ src/components/templates/MainGnb.tsx
@@ -22,12 +22,41 @@ const Container = styled.div`
     min-height: calc(100vh - ${({ theme }) => theme.size.headerHeight});
 `;
 
-const LeftPanel = styled(LeftPanelBase)`
+const LeftPanelContainer = styled.div`
+    position: relative;
+
     width: 25vw;
-    flex-basis: 25%;
     max-width: 32rem;
     min-width: 28rem;
+
+    min-height: inherit;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: stretch;
+    flex-basis: 25%;
     flex-grow: 0;
+`;
+
+const LeftPanelSubContainer = styled.div`
+    position: fixed;
+
+    display: flex;
+    width: inherit;
+    min-width: inherit;
+    max-width: inherit;
+    height: 0;
+    min-height: inherit;
+    max-height: 0;
+`;
+
+const LeftPanel = styled(LeftPanelBase)`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-content: stretch;
+    overflow-y: scroll;
+    flex-grow: 1;
 `;
 
 const Contents = styled.div`
@@ -39,7 +68,14 @@ const MainGnb: FC = ({ children }: PropsWithChildren<{}>) => (
     <div>
         <Header />
         <Container>
-            <LeftPanel />
+            {/* 좌측 네비게이션 */}
+            <LeftPanelContainer>
+                <LeftPanelSubContainer>
+                    <LeftPanel />
+                </LeftPanelSubContainer>
+            </LeftPanelContainer>
+
+            {/* 내용 */}
             <Contents>{children}</Contents>
         </Container>
     </div>
