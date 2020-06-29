4cdec40d604518819dfc0e647d797b132d820ba8
wtae1216 <wtae1216@gmail.com>
2020-06-30 01:32:49 +0900

    GroupsMock.ts 추가  - 페이징을 위한 group 리스트 3번쨰 까지 추가

--- src/mocks/GroupsMock.ts
+++ src/mocks/GroupsMock.ts
@@ -1,10 +1,27 @@
 import { TApiMock } from 'mocks/index';
 
-import groups from 'data/groups.json';
+import groups1 from 'data/groups/get_1.json';
+import groups2 from 'data/groups/get_2.json';
+import groups3 from 'data/groups/get_3.json';
 
 // 그룹 리스트 조회
 const mockGetGroupList: TApiMock = (apiMock) => {
-    apiMock.onGet('/groups').reply(200, groups);
+    apiMock.onGet('/groups').reply((config) => {
+        const {
+            params: { last }
+        } = config;
+
+        switch (true) {
+            case last === null || last < 10:
+                return [200, groups1];
+            case last < 20:
+                return [200, groups2];
+            case last < 30:
+                return [200, groups3];
+            default:
+                return [515, { message: '그룹이 없습니다.' }];
+        }
+    });
 };
 
 const GroupsMock: TApiMock = (apiMock) => {
