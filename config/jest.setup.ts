dad7e36f115fb7bca7c0c0c364debc05d4ffb7cd
wtae1216 <wtae1216@gmail.com>
2020-07-16 02:40:45 +0900

    jest 버전 업 jest 기본 api mock 적용

--- config/jest.setup.ts
+++ config/jest.setup.ts
@@ -1,6 +1,15 @@
 import '@testing-library/jest-dom/extend-expect';
 import { matchers } from 'jest-emotion';
 
+import { initApiMock } from 'mocks';
+
+import { axiosMock } from 'libs/axios';
+
+// emotion style 테스트를 위한 matcher 추가
 expect.extend(matchers);
 
+// 테스트 시 mocking 을 안할 경우 기본 mocking 추가
+initApiMock(axiosMock({ delayResponse: 300 }));
+
+// jest 타입아웃 30초로 설정
 jest.setTimeout(30000);
