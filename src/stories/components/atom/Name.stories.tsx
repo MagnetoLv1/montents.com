7a54c21495aacfa0524ad7c9306c89949e874535
wtae1216 <wtae1216@gmail.com>
2020-07-26 19:15:46 +0900

    Name 컴포넌트 스토리 수정

--- src/stories/components/atom/Name.stories.tsx
+++ src/stories/components/atom/Name.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 import { text, withKnobs } from '@storybook/addon-knobs';
 
 import Name from 'components/atoms/Name';
@@ -13,7 +14,14 @@ export default {
 export const name = () => {
     const name = text('name', 'SeoHwan Cho');
 
-    return <Name text={name} />;
+    return (
+        <Name
+            text={name}
+            onClick={action('onClick')}
+            onMouseEnter={action('onMouseEnter')}
+            onMouseLeave={action('onMouseLeave')}
+        />
+    );
 };
 
 name.story = {
