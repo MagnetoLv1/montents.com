68fb88139244e76775a7ac1e8ed391b0b860733e
wtae1216 <wtae1216@gmail.com>
2020-07-30 01:09:30 +0900

    게시판 타입 정의

---
+++ src/types/response/IBoard.ts
@@ -0,0 +1,23 @@
+import IGroup, { isGroup } from 'types/response/IGroup';
+
+interface IBoard {
+    idx: number;
+    title: string;
+    content: string;
+    url: string;
+    created_ad: string;
+    group: IGroup;
+}
+
+export const isBoard = (item: any): item is IBoard => {
+    return (
+        typeof item.idx === 'number' &&
+        typeof item.title === 'string' &&
+        typeof item.content === 'string' &&
+        typeof item.url === 'string' &&
+        typeof item.created_ad === 'string' &&
+        isGroup(item)
+    );
+};
+
+export default IBoard;
