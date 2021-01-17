8cff3abcdc0b37105c50e155c768ad0599c6e386
wtae1216 <wtae1216@gmail.com>
2021-01-17 15:18:31 +0900

    로딩이 필요한 컴포넌트를 위한 withLoading HOC 개발
     - 로딩 상태에 따라 다른 컴포넌트 반환

---
+++ src/libs/hoc/withLoading.test.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+
+import render from 'libs/testUtils';
+import withLoading from 'libs/hoc/withLoading';
+
+describe('Libs | HOC | withLoading', () => {
+    const loadingText = 'loading...';
+    const loadedText = 'loaded';
+
+    const renderLoadableComponent = (loading = false) => {
+        const Loading: FC = () => <div>{loadingText}</div>;
+        const Loaded: FC = () => <div>{loadedText}</div>;
+
+        const LoadableComponent = withLoading(Loaded, Loading);
+        return render(<LoadableComponent loading={loading} />);
+    };
+
+    it('로딩중인 컴포넌트 노출', () => {
+        const { getByText } = renderLoadableComponent(true);
+        expect(getByText(loadingText)).toBeInTheDocument();
+    });
+
+    it('로딩 완료된 컴포넌트 노출', () => {
+        const { getByText } = renderLoadableComponent();
+        expect(getByText(loadedText)).toBeInTheDocument();
+    });
+});
