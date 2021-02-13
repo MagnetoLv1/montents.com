119c0cd6dc1d79ce5e278c03c41e5ad5d7eead39
wtae1216 <wtae1216@gmail.com>
2021-02-13 09:20:42 +0900

    실서버 환경이 아닐경우에 메시지 노출하도록 변경

--- src/errors/CommonError.ts
+++ src/errors/CommonError.ts
@@ -1,9 +1,9 @@
-import { isDev } from 'constants/env';
+import { isProd } from 'constants/env';
 import Exceptions from 'constants/Exceptions';
 
 class CommonError extends Error {
     constructor(message?: string | null) {
-        if (typeof message !== 'string' || !isDev) message = Exceptions.ERROR;
+        if (typeof message !== 'string' || isProd) message = Exceptions.ERROR;
 
         super(message);
         this.name = 'CommonError';
