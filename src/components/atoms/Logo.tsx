87e742bc6518fc1baa25d519023353c10a6cc298
wtae1216 <wtae1216@gmail.com>
2021-04-13 03:31:21 +0900

    Logo 스타일 변경

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -4,19 +4,19 @@ import styled from '@emotion/styled';
 
 import LogoSvg from 'assets/images/logo.svg';
 
-import IconBase from 'components/atoms/Icon';
+import Icon from 'components/atoms/Icon';
 
-const Icon = styled(IconBase)`
-    width: 40px;
-    height: 40px;
+const IconLink = styled(Link)`
+    width: 4rem;
+    height: 4rem;
 `;
 
 interface LogoProps extends Omit<LinkProps, 'to'> {}
 
 const Logo: FC<LogoProps> = (props: LogoProps) => (
-    <Link to="/" {...props}>
+    <IconLink to="/" {...props}>
         <Icon src={LogoSvg} alt="montents" title="logo" />
-    </Link>
+    </IconLink>
 );
 
 export default Logo;
